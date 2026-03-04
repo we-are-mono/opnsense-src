4c9949f2bb00b23cdf1e8388d5704f30fd2fe9fa
Tomaz Zaman <tomaz@mono.si>
2026-03-04 21:51:09 +0100

    pf: add state creation/destruction notification hooks
    Add callback interface for PF state lifecycle events (create, destroy,
    update). Used by CMM connection offload module to synchronize hardware
    flow tables with PF firewall state.

--- sys/netpfil/pf/pf_ioctl.c
+++ sys/netpfil/pf/pf_ioctl.c
@@ -299,6 +299,11 @@ VNET_DEFINE(pfsync_defer_t *, pfsync_defer_ptr);
 VNET_DEFINE(pflow_export_state_t *, pflow_export_state_ptr);
 pfsync_detach_ifnet_t *pfsync_detach_ifnet_ptr;
 
+/* pf_notify */
+VNET_DEFINE(pfnotify_insert_state_t *, pfnotify_insert_state_ptr);
+VNET_DEFINE(pfnotify_update_state_t *, pfnotify_update_state_ptr);
+VNET_DEFINE(pfnotify_delete_state_t *, pfnotify_delete_state_ptr);
+
 /* pflog */
 pflog_packet_t			*pflog_packet_ptr = NULL;
 
