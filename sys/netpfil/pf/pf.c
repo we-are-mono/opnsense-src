5abae0f807307e54703519447ef6165ce8f50fa0
Tomaz Zaman <tomaz@mono.si>
2026-03-04 21:51:09 +0100

    pf: add state creation/destruction notification hooks
    Add callback interface for PF state lifecycle events (create, destroy,
    update). Used by CMM connection offload module to synchronize hardware
    flow tables with PF firewall state.

--- sys/netpfil/pf/pf.c
+++ sys/netpfil/pf/pf.c
@@ -1670,6 +1670,8 @@ pf_state_insert(struct pfi_kkif *kif, struct pfi_kkif *orig_kif,
 	pf_counter_u64_add(&V_pf_status.fcounters[FCNT_STATE_INSERT], 1);
 	if (V_pfsync_insert_state_ptr != NULL)
 		V_pfsync_insert_state_ptr(s);
+	if (V_pfnotify_insert_state_ptr != NULL)
+		V_pfnotify_insert_state_ptr(s);
 
 	/* Returns locked. */
 	return (0);
@@ -2457,6 +2459,8 @@ pf_unlink_state(struct pf_kstate *s)
 
 	if (V_pfsync_delete_state_ptr != NULL)
 		V_pfsync_delete_state_ptr(s);
+	if (V_pfnotify_delete_state_ptr != NULL)
+		V_pfnotify_delete_state_ptr(s);
 
 	STATE_DEC_COUNTERS(s);
 
@@ -8652,6 +8656,8 @@ pf_test(int dir, int pflags, struct ifnet *ifp, struct mbuf **m0,
 			if (s != NULL) {
 				if (V_pfsync_update_state_ptr != NULL)
 					V_pfsync_update_state_ptr(s);
+				if (V_pfnotify_update_state_ptr != NULL)
+					V_pfnotify_update_state_ptr(s);
 				r = s->rule.ptr;
 				a = s->anchor.ptr;
 			}
@@ -8717,6 +8723,8 @@ pf_test(int dir, int pflags, struct ifnet *ifp, struct mbuf **m0,
 			if (s != NULL) {
 				if (V_pfsync_update_state_ptr != NULL)
 					V_pfsync_update_state_ptr(s);
+				if (V_pfnotify_update_state_ptr != NULL)
+					V_pfnotify_update_state_ptr(s);
 				r = s->rule.ptr;
 				a = s->anchor.ptr;
 			}
@@ -8750,6 +8758,8 @@ pf_test(int dir, int pflags, struct ifnet *ifp, struct mbuf **m0,
 		if (action == PF_PASS) {
 			if (V_pfsync_update_state_ptr != NULL)
 				V_pfsync_update_state_ptr(s);
+			if (V_pfnotify_update_state_ptr != NULL)
+				V_pfnotify_update_state_ptr(s);
 			r = s->rule.ptr;
 			a = s->anchor.ptr;
 		} else if (s == NULL) {
@@ -8770,6 +8780,8 @@ pf_test(int dir, int pflags, struct ifnet *ifp, struct mbuf **m0,
 		if (action == PF_PASS) {
 			if (V_pfsync_update_state_ptr != NULL)
 				V_pfsync_update_state_ptr(s);
+			if (V_pfnotify_update_state_ptr != NULL)
+				V_pfnotify_update_state_ptr(s);
 			r = s->rule.ptr;
 			a = s->anchor.ptr;
 		} else if (s == NULL)
@@ -8792,6 +8804,8 @@ pf_test(int dir, int pflags, struct ifnet *ifp, struct mbuf **m0,
 		if (action == PF_PASS) {
 			if (V_pfsync_update_state_ptr != NULL)
 				V_pfsync_update_state_ptr(s);
+			if (V_pfnotify_update_state_ptr != NULL)
+				V_pfnotify_update_state_ptr(s);
 			r = s->rule.ptr;
 			a = s->anchor.ptr;
 		} else if (s == NULL)
@@ -9310,6 +9324,8 @@ pf_test6(int dir, int pflags, struct ifnet *ifp, struct mbuf **m0, struct inpcb 
 		if (action == PF_PASS) {
 			if (V_pfsync_update_state_ptr != NULL)
 				V_pfsync_update_state_ptr(s);
+			if (V_pfnotify_update_state_ptr != NULL)
+				V_pfnotify_update_state_ptr(s);
 			r = s->rule.ptr;
 			a = s->anchor.ptr;
 		} else if (s == NULL) {
@@ -9374,6 +9390,8 @@ pf_test6(int dir, int pflags, struct ifnet *ifp, struct mbuf **m0, struct inpcb 
 		if (action == PF_PASS) {
 			if (V_pfsync_update_state_ptr != NULL)
 				V_pfsync_update_state_ptr(s);
+			if (V_pfnotify_update_state_ptr != NULL)
+				V_pfnotify_update_state_ptr(s);
 			r = s->rule.ptr;
 			a = s->anchor.ptr;
 		} else if (s == NULL)
@@ -9404,6 +9422,8 @@ pf_test6(int dir, int pflags, struct ifnet *ifp, struct mbuf **m0, struct inpcb 
 		if (action == PF_PASS) {
 			if (V_pfsync_update_state_ptr != NULL)
 				V_pfsync_update_state_ptr(s);
+			if (V_pfnotify_update_state_ptr != NULL)
+				V_pfnotify_update_state_ptr(s);
 			r = s->rule.ptr;
 			a = s->anchor.ptr;
 		} else if (s == NULL) {
@@ -9431,6 +9451,8 @@ pf_test6(int dir, int pflags, struct ifnet *ifp, struct mbuf **m0, struct inpcb 
 		if (action == PF_PASS) {
 			if (V_pfsync_update_state_ptr != NULL)
 				V_pfsync_update_state_ptr(s);
+			if (V_pfnotify_update_state_ptr != NULL)
+				V_pfnotify_update_state_ptr(s);
 			r = s->rule.ptr;
 			a = s->anchor.ptr;
 		} else if (s == NULL)
@@ -9444,6 +9466,8 @@ pf_test6(int dir, int pflags, struct ifnet *ifp, struct mbuf **m0, struct inpcb 
 		if (action == PF_PASS) {
 			if (V_pfsync_update_state_ptr != NULL)
 				V_pfsync_update_state_ptr(s);
+			if (V_pfnotify_update_state_ptr != NULL)
+				V_pfnotify_update_state_ptr(s);
 			r = s->rule.ptr;
 			a = s->anchor.ptr;
 		} else if (s == NULL)
